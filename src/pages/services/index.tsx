489dd1948f92cf277796a9d900929528ab477e9f
alvin leung <alvinleung2009@gmail.com>
2024-10-30 10:43:50 -0700

    add vertical carousel

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,7 +1,10 @@
 import Navigation from "@/components/navigation";
+import Image from "next/image";
 import { client } from "@/sanity/lib/client";
 import { GetStaticProps } from "next";
 import type { Services } from "@/sanity/types";
+import Stack from "./components/Stack";
+import StackGroupVertical from "./components/StackGroupVertical";
 
 export default function Services({ data }: { data: Services }) {
   console.log(data);
@@ -9,8 +12,32 @@ export default function Services({ data }: { data: Services }) {
   return (
     <div>
       <div className="h-[100vh] bg-red-200"></div>
-      <div className="p-96 text-center">Insert your section here.</div>
-      <div className="h-[100vh] bg-blue-200"></div>
+      <StackGroupVertical>
+        {Object.entries(data.serviceCategories!).map((category, index) => {
+          const categoryName = category[0];
+          const tabs = category[1].tabs!;
+          return (
+            <Stack
+              className="flex max-h-[98vh] max-w-[90vw]"
+              index={index}
+              key={index}
+            >
+              <div className="w-[30%]">
+                {tabs[0].heading}
+                {tabs[0].copy}
+              </div>
+              <Image
+                className=""
+                src={tabs[0].image?.asset?._ref!}
+                alt={tabs[0].copy!}
+                width={597}
+                height={597}
+              />
+            </Stack>
+          );
+        })}
+      </StackGroupVertical>
+      {/* <div className="h-[100vh] bg-blue-200"></div> */}
     </div>
   );
 }
