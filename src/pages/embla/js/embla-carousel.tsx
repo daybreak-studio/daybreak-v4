e73737409f7c155e61dd514c6867c80ee399dcc9
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2024-12-03 16:08:57 +0000

    feat: add motion bio

--- src/pages/embla/js/embla-carousel.tsx
+++ src/pages/embla/js/embla-carousel.tsx
@@ -1,4 +1,6 @@
-import React, { useCallback, useEffect, useRef } from "react";
+"use client";
+
+import React, { use, useCallback, useEffect, useRef } from "react";
 import {
   EmblaCarouselType,
   EmblaEventType,
@@ -11,6 +13,10 @@ import {
   usePrevNextButtons,
 } from "./embla-carousel-arrow-buttons";
 import { DotButton, useDotButton } from "./embla-carousel-dot-button";
+import { cn } from "@/lib/utils";
+import { relative } from "path";
+import { motion, AnimatePresence, easeInOut } from "framer-motion";
+import { v4 as uuidv4 } from "uuid";
 
 const TWEEN_SCALE_FACTOR_BASE = 0.2;
 const TWEEN_OPACITY_FACTOR_BASE = 0.0;
@@ -21,12 +27,20 @@ const numberWithinRange = (number: number, min: number, max: number): number =>
 
 interface Person {
   id: string;
-  position: string;
   name: string;
+  role: string;
   bio: string;
+  info: [
+    {
+      question: string;
+      answer: string;
+    },
+    {
+      question: string;
+      answer: string;
+    },
+  ];
   video: string;
-  trait: string;
-  birthdate: string;
 }
 
 type PropType = {
@@ -236,47 +250,188 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
     tweenBlur,
   ]);
 
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <div className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {people.map((person) => (
-            <div className="embla__slide" key={person.id}>
-              <div className="embla__slide__number">
-                <video
-                  autoPlay
-                  muted
-                  loop
-                  className="h-full w-fit mix-blend-multiply"
-                >
-                  <source src={person.video} type="video/mp4" />
-                </video>
+    <React.Fragment>
+      <div className="embla">
+        <div className="gradient" />
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {people.map((person) => (
+              <div className={cn("embla__slide")} key={person.id}>
+                <div className={cn("embla__slide__number", "relative")}>
+                  <video
+                    autoPlay
+                    muted
+                    loop
+                    className="h-full w-fit object-cover mix-blend-multiply"
+                  >
+                    <source src={person.video} type="video/mp4" />
+                  </video>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
 
-      {/* 
-        <div className="embla__controls">
-          <div className="embla__buttons">
-            <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-            <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-          </div>
+      <div className="absolute bottom-8 z-10 flex w-screen items-center">
+        <div className="relative flex w-full items-center justify-around">
+          <AnimatePresence mode="popLayout">
+            {!open && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.8 }}
+                transition={{ duration: 0.2 }}
+                className="absolute bottom-24"
+              >
+                <EmblaCarouselDots
+                  emblaApi={emblaApi}
+                  onDotButtonClick={onDotButtonClick}
+                />
+              </motion.div>
+            )}
+          </AnimatePresence>
 
-          <div className="embla__dots">
-            {scrollSnaps.map((_, index) => (
-              <DotButton
-                key={index}
-                onClick={() => onDotButtonClick(index)}
-                className={"embla__dot".concat(
-                  index === selectedIndex ? "embla__dot--selected" : "",
-                )}
-              />
-            ))}
+          <motion.div
+            layoutRoot
+            onClick={() => setOpen(!open)}
+            whileTap={{ scale: 0.95 }}
+            className="absolute bottom-0 transform cursor-pointer"
+          >
+            <EmblaCarouselBio person={people[selectedIndex]} open={open} />
+          </motion.div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+interface EmblaCarouselBioProps {
+  person: Person;
+  open: boolean;
+}
+
+const EmblaCarouselBio: React.FC<EmblaCarouselBioProps> = (props) => {
+  const { person, open } = props;
+
+  return (
+    <motion.div
+      layout
+      layoutId="person-container"
+      className="flex w-[344px] cursor-pointer flex-col items-center justify-between gap-6 overflow-hidden bg-white/50 p-4 align-middle backdrop-blur-xl"
+      style={{
+        borderRadius: "16px",
+      }}
+    >
+      <AnimatePresence mode="popLayout">
+        {open ? (
+          <div className="flex gap-2 text-[length:var(--Spacing-35,14px)] font-[450] not-italic leading-[var(--Spacing-6,24px)] text-[color(display-p3_0_0_0)] text-black">
+            <motion.div
+              key={person.name}
+              initial={{ filter: "blur(2px)", x: 6 }}
+              animate={{ filter: "blur(0px)", x: 0 }}
+              exit={{ filter: "blur(2px)" }}
+              layout="position"
+              className="text-black opacity-60"
+            >
+              {person.name}
+            </motion.div>
+            <motion.div
+              key={person.role}
+              initial={{ filter: "blur(2px)", x: 4 }}
+              animate={{ filter: "blur(0px)", x: 0 }}
+              exit={{ filter: "blur(2px)" }}
+              layout="position"
+              className="text-black opacity-40"
+            >
+              {person.role}
+            </motion.div>
           </div>
-        </div> 
-      */}
+        ) : (
+          <div className="flex w-full flex-col text-[length:var(--Spacing-35,14px)] font-[450] not-italic leading-[var(--Spacing-6,24px)] text-[color(display-p3_0_0_0)] text-black">
+            <motion.div
+              key={person.name}
+              initial={{ filter: "blur(2px)", x: 6 }}
+              animate={{ filter: "blur(0px)", x: 0 }}
+              exit={{ filter: "blur(2px)" }}
+              layout="position"
+              className="text-black opacity-60"
+            >
+              {person.name}
+            </motion.div>
+            <motion.div
+              key={person.role}
+              initial={{ filter: "blur(2px)", x: 4 }}
+              animate={{ filter: "blur(0px)", x: 0 }}
+              exit={{ filter: "blur(2px)" }}
+              layout="position"
+              className="text-black opacity-40"
+            >
+              {person.role}
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence mode="popLayout">
+        {open && (
+          <motion.div
+            initial={{ opacity: 0, filter: "blur(8px)" }}
+            animate={{ opacity: 1, filter: "blur(0px)" }}
+            exit={{ opacity: 0, filter: "blur(8px)" }}
+            transition={{ duration: 0.2 }}
+            className="flex flex-col gap-6"
+            layout="position"
+          >
+            <div className="px-8 text-center text-lg font-[450] not-italic leading-[var(--Spacing-7,28px)] text-[color(display-p3_0_0_0)] text-black opacity-50">
+              {person.bio}
+            </div>
+            <div className="flex items-start gap-1 self-stretch">
+              {person.info.map((info, index) => (
+                <div
+                  key={index}
+                  className="flex h-[150px] w-[150px] flex-[1_0_0] flex-col items-center justify-center rounded-2xl bg-black/[0.03] px-5 py-10"
+                >
+                  <div className="text-center text-[length:var(--Spacing-35,14px)] font-[350] not-italic leading-[var(--Spacing-6,24px)] text-[color(display-p3_0_0_0)] text-black opacity-40">
+                    {info.question}
+                  </div>
+                  <div className="text-[length:var(--Spacing-5,20px)] font-[450] not-italic leading-[var(--Spacing-8,32px)] text-[color(display-p3_0_0_0)] text-black opacity-60">
+                    {info.answer}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
+  );
+};
+
+interface EmblaCarouselDotsProps {
+  emblaApi: EmblaCarouselType | undefined;
+  onDotButtonClick: (index: number) => void;
+}
+
+const EmblaCarouselDots: React.FC<EmblaCarouselDotsProps> = (props) => {
+  const { emblaApi, onDotButtonClick } = props;
+  const { selectedIndex, scrollSnaps } = useDotButton(emblaApi);
+
+  return (
+    <div className="inline-flex items-center gap-2">
+      {scrollSnaps.map((_, index) => (
+        <DotButton
+          key={index}
+          onClick={() => onDotButtonClick(index)}
+          className={cn(
+            "h-1 w-1 rounded-full bg-[color(display-p3_0_0_0)] opacity-20",
+            "mr-2",
+            index === selectedIndex && "scale-150 opacity-100",
+          )}
+        />
+      ))}
     </div>
   );
 };
